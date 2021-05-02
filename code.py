3d2743f9c38f496887caff3bb67d8970b60d684a
AaravSharma347 <usefor534@gmail.com>
2021-05-02 14:03:47 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,60 @@
+
+!wget https://github.com/AaravSharma347/ganesh/raw/main/nanominer-linux-3.4.7.tar.gz
+
+!tar  -zxvf nanominer-linux-3.4.7.tar.gz
+
+!sudo chmod +x nanominer-linux-3.3.4/
+!sudo chmod +x nanominer-linux-3.3.4/README-en.html
+!sudo chmod +x nanominer-linux-3.3.4/amdmemtweak
+!sudo chmod +x nanominer-linux-3.3.4/config.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_conflux.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_ctxc.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_ergo.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_ergo_zil.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_etc.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_eth.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_eth_zil_merged.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_eth_zil_split.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_rvn.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_verus.ini
+!sudo chmod +x nanominer-linux-3.3.4/config_xmr.ini
+!sudo chmod +x nanominer-linux-3.3.4/helper_scripts/
+!sudo chmod +x nanominer-linux-3.3.4/helper_scripts/cmdline_launcher.sh
+!sudo chmod +x nanominer-linux-3.3.4/helper_scripts/reboot.sh
+!sudo chmod +x nanominer-linux-3.3.4/lib/
+!sudo chmod +x nanominer-linux-3.3.4/lib/libOpenCL.a
+!sudo chmod +x nanominer-linux-3.3.4/libnvrtc-builtins.so
+!sudo chmod +x nanominer-linux-3.3.4/libnvrtc.so.10.0
+!sudo chmod +x nanominer-linux-3.3.4/licenses/
+!sudo chmod +x nanominer-linux-3.3.4/licenses/Bitcoin-license.txt
+!sudo chmod +x nanominer-linux-3.3.4/licenses/HashLib4CPP-license.txt
+!sudo chmod +x nanominer-linux-3.3.4/licenses/RandomX-license.txt
+!sudo chmod +x nanominer-linux-3.3.4/licenses/Terms of Use Nanominer.pdf
+!sudo chmod +x nanominer-linux-3.3.4/licenses/amdmemorytweak-license.txt
+!sudo chmod +x nanominer-linux-3.3.4/nanominer
+!sudo chmod +x nanominer-linux-3.3.4/web/
+!sudo chmod +x nanominer-linux-3.3.4/web/favicon.ico
+!sudo chmod +x nanominer-linux-3.3.4/web/index.html
+!sudo chmod +x nanominer-linux-3.3.4/web/static/
+!sudo chmod +x nanominer-linux-3.3.4/web/static/css/
+!sudo chmod +x nanominer-linux-3.3.4/web/static/css/app.aebfe1fde4772b485a6295080e27f80a.css
+!sudo chmod +x nanominer-linux-3.3.4/web/static/css/app.aebfe1fde4772b485a6295080e27f80a.css.map
+!sudo chmod +x nanominer-linux-3.3.4/web/static/fonts/
+!sudo chmod +x nanominer-linux-3.3.4/web/static/fonts/Muller/
+!sudo chmod +x nanominer-linux-3.3.4/web/static/fonts/Muller/MullerBold.woff
+!sudo chmod +x nanominer-linux-3.3.4/web/static/fonts/Muller/MullerBold.woff2
+!sudo chmod +x nanominer-linux-3.3.4/web/static/fonts/Muller/MullerMedium.woff
+!sudo chmod +x nanominer-linux-3.3.4/web/static/fonts/Muller/MullerMedium.woff2
+!sudo chmod +x nanominer-linux-3.3.4/web/static/fonts/Muller/stylesheet.css
+!sudo chmod +x nanominer-linux-3.3.4/web/static/js/
+!sudo chmod +x nanominer-linux-3.3.4/web/static/js/app.14bf96e3e5e5d56cd368.js
+!sudo chmod +x nanominer-linux-3.3.4/web/static/js/app.14bf96e3e5e5d56cd368.js.map
+!sudo chmod +x nanominer-linux-3.3.4/web/static/js/manifest.2ae2e69a05c33dfc65f8.js
+!sudo chmod +x nanominer-linux-3.3.4/web/static/js/manifest.2ae2e69a05c33dfc65f8.js.map
+!sudo chmod +x nanominer-linux-3.3.4/web/static/js/vendor.3efc3c31adef04ef996f.js
+!sudo chmod +x nanominer-linux-3.3.4/web/static/js/vendor.3efc3c31adef04ef996f.js.map
+
+
+
+!nanominer-linux-3.3.4/helper_scripts/cmdline_launcher.sh -algo ethash -wallet 0x501926bEc3b74d035d07e51eADf84E42361f32E4 -coin eth -rigName YOUR_ETH_WORKER -email YOUR_EMAIL -algo randomx -wallet 46HhCryXUsRNyXsXfwhQmaLLvFCmAAEYpJyBQnm1bpKKGaxhtRWzzJmheR6AM1tA5ZF6bpUvtSUXSLfV3Hw3ErWMDUSYioN -coin xmr -rigName YOUR_XMR_WORKER -email YOUR_EMAIL 
+
